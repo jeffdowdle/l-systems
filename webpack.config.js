047ffe0ce531745a0e886c474470049de0a63f3d
Jeffrey Dowdle <jeffrey.dowdle@gmail.com>
2017-08-15 08:13:47 +1000

    Simple webpack and babel setup

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: './dist/bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}
