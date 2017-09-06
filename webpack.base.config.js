bdffafdae9ac18ac8e26daa63a7c50990d095a63
Jeffrey Dowdle <jeffrey.dowdle@gmail.com>
2017-09-06 20:25:23 +1000

    Added build script, css in seperate file

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const DIST_PATH = path.resolve(__dirname, 'dist');
 
 module.exports = {
+  stats: {
+    children: false,
+  },
   entry: './src/index.js',
   output: {
     path: DIST_PATH,
@@ -30,18 +35,27 @@ module.exports = {
       {
         include: path.resolve(__dirname, './src'),
         exclude: path.resolve(__dirname, './src/styles/global.scss'),
-        use: [
-          'style-loader',
-          'css-loader?importLoader=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-          'postcss-loader',
-          'sass-loader',
-          {
-            loader: 'sass-resources-loader',
-            options: {
-              resources: ['./src/styles/resources/index.scss'],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoader: 1,
+                modules: true,
+                localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+              },
             },
-          },
-        ],
+            'postcss-loader',
+            'sass-loader',
+            {
+              loader: 'sass-resources-loader',
+              options: {
+                resources: ['./src/styles/resources/index.scss'],
+              },
+            },
+          ],
+        }),
         test: /\.(css|scss)$/,
       },
 
@@ -57,6 +71,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.ejs',
+    }),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
+      },
     }),
   ],
 };
